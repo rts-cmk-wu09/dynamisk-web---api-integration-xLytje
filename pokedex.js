96a6196be8d8b996dbc37b41b06e3b22e0639ad4
xLytje <rasmuslytje@gmail.com>
2023-03-16 09:46:32 +0100

    updated

--- pokedex.js
+++ pokedex.js
@@ -1,17 +1,20 @@
+const params = new URLSearchParams(window.location.search);
+const name = params.get("name");
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+fetch(`https://pokeapi.co/api/v2/pokemon/`)
     .then((response) => response.json())
     .then((data) => {
 
-        document.querySelector("body").innerHTML += `
+        document.querySelector(".entry").innerHTML += `
         <div class="listed">
-            <div>
-                <img src="img/bulbasaur.png">
-                <h3>${name}</h3>
-            </div>
-            <p>Vis mere</p>
-            <img src="img/pokeball.png" alt="Pokéball billede">
-        </div>`;
+        <div>
+            <img src="img/bulbasaur.png">
+            <h3>Bulbasaur</h3>
+        </div>
+        <p>Vis mere</p>
+        <img src="img/pokeball.png" alt="Pokéball billede">
+        </div> 
+        `;
     });
 
 
