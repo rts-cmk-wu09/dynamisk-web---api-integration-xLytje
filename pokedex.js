8713482cf0612a24dc8c3f2c7c97899b29754845
xLytje <rasmuslytje@gmail.com>
2023-03-16 09:34:54 +0100

    fetch testing

--- pokedex.js
+++ pokedex.js
@@ -1,8 +1,17 @@
 
-fetch("https://pokeapi.co/api/v2/pokemon")
+fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
     .then((data) => {
 
+        document.querySelector("body").innerHTML += `
+        <div class="listed">
+            <div>
+                <img src="img/bulbasaur.png">
+                <h3>${name}</h3>
+            </div>
+            <p>Vis mere</p>
+            <img src="img/pokeball.png" alt="Pokéball billede">
+        </div>`;
     });
 
 
