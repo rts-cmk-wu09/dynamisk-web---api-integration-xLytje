c2618b1f90a2a0b7c0d6e9c88890fccf23f8ae0e
xLytje <rasmuslytje@gmail.com>
2023-03-16 11:30:50 +0100

    tessting for sorting correctly

--- pokedex.js
+++ pokedex.js
@@ -2,7 +2,6 @@ fetch(`https://pokeapi.co/api/v2/pokemon/?limit=151`)
     .then((response) => response.json())
     .then((data) => {
         data.results.forEach((pokemon) => {
-            console.log(pokemon)
             fetch(`${pokemon.url}`)
                 .then((response) => response.json())
                 .then((data) => {
