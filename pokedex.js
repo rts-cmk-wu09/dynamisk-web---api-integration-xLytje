3a504e19169636cf44ab23fa7ef4bb7cb76f4219
xLytje <rasmuslytje@gmail.com>
2023-03-16 09:21:21 +0100

    start of dynamic and fetch

---
+++ pokedex.js
@@ -0,0 +1,26 @@
+
+fetch("https://pokeapi.co/api/v2/pokemon")
+    .then((response) => response.json())
+    .then((data) => {
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+/* <div class="listed">
+     <div>
+         <img src="img/bulbasaur.png">
+         <h3>Bulbasaur</h3>
+     </div>
+     <p>Vis mere</p>
+     <img src="img/pokeball.png" alt="Pokéball billede">
+</div> */
