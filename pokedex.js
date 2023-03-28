46978ab2ca7f163ff5494ee84cf2c0e6a04543a8
xLytje <rasmuslytje@gmail.com>
2023-03-28 12:27:42 +0200

    small changes

--- pokedex.js
+++ pokedex.js
@@ -6,28 +6,44 @@ fetch(`https://pokeapi.co/api/v2/pokemon/?limit=151`)
         });
 
         Promise.all(promises).then((pokemons) => {
-            pokemons.forEach((pokemon, pokemonIndex) => {
-                console.log("foreach: " + pokemonIndex);
-                document.querySelector(".entry").innerHTML += `
+            const entryHTML = pokemons.map((pokemon, pokemonIndex) => `
                 <div class="listed">
-                <div>
-                    <img src="${pokemon.sprites.front_default}">
-                    <h3>${pokemon.name}</h3>
-                    <p>#${pokemon.game_indices[9].game_index}</p>
-                    ${pokemon.abilities.map(item => `${item.ability.name}`).join(" - ")}
+                    <div>
+                        <img src="${pokemon.sprites.front_default}">
+                        <h3>${pokemon.name}</h3>
+                        <p>#${pokemon.game_indices[9].game_index}</p>
+                    </div>
+                    <img src="img/pokeball.png" alt="Pokéball billede">
                 </div>
-                <img src="img/pokeball.png" alt="Pokéball billede">
-                </div> 
-                `;
-            });
+            `).join("");
+
+            document.querySelector(".entry").innerHTML = entryHTML;
 
             const listedItems = document.querySelectorAll(".listed");
+            
             listedItems.forEach((item, listedItemIndex) => {
-                console.log("listedItemIndex: " + listedItemIndex);
                 item.addEventListener("click", () => {
                     const newDiv = document.createElement("div");
                     newDiv.classList.add("info");
-                    newDiv.textContent = pokemon.abilities.map(item => `${item.ability.name}`);                            
+                    newDiv.innerHTML = "";
+
+                    const title = document.createElement("h3");
+                    title.textContent = "Abilities:";
+                    newDiv.appendChild(title);
+                        
+                    const newP = document.createElement("p");
+                    newP.classList.add("abilities");
+                    newP.innerHTML = "";
+
+                    
+
+                    newP.innerHTML = pokemons[listedItemIndex].abilities.join(", ")
+                    pokemons[listedItemIndex].abilities.forEach((ability) => {
+                        const p = document.createElement("p");
+                        p.textContent = ability.ability.name;
+                        newP.appendChild(p);
+                    });
+
                     const isAdded = item.classList.contains("added");
                     if (isAdded) {
                         const addedDiv = document.querySelector(".info");
@@ -39,5 +55,6 @@ fetch(`https://pokeapi.co/api/v2/pokemon/?limit=151`)
                     }
                 });
             });
+            
         });
     });
